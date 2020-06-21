8ba0e983b609d911d5b13ea5c60d39936794cdd7
오형남 <zzzsh789@naver.com>
2020-06-21 15:51:42 +0900

    Update: ToDo.js 스타일 추가, 수정/완료버튼 추가

--- ToDo.js
+++ ToDo.js
@@ -3,25 +3,63 @@ import { Text, StyleSheet, View, Dimensions, TouchableOpacity } from 'react-nati
 
 const { width, height } = Dimensions.get('window');
 const ToDo = () => {
-    const [isEditing, setisEditing] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
     const [isCompleted, setIsCompleted] = useState(false);
 
     const toggleComplete = () => {
+        setIsEditing(false);
         setIsCompleted(!isCompleted);
     }
-    console.log(isCompleted);
+
+    const onClickEditing = () => {
+        setIsEditing(true);
+    }
+
+    const onClickSave = () => {
+        setIsEditing(false);
+    }
+
+    const onClickDelete = () => {
+    }
     return (
-        <View
-            style={[
-                styles.container,
-            ]}>
-            <TouchableOpacity onPress={toggleComplete}>
-                <View
-                    style={[styles.circle,
-                    isCompleted ? styles.completedCircle : styles.unCompletedCircle
-                    ]} />
-            </TouchableOpacity>
-            <Text style={styles.text}>안녕 나는 투두야 ^.^</Text>
+        <View style={styles.container}>
+            <View style={styles.column}>
+                <TouchableOpacity onPress={toggleComplete}>
+                    <View
+                        style={[styles.circle,
+                        isCompleted ? styles.completedCircle : styles.unCompletedCircle
+                        ]} />
+                </TouchableOpacity>
+                <Text
+                    style={[
+                        styles.text,
+                        isCompleted ? styles.completedText : styles.unCompletedText,
+                    ]}>안녕 나는 투두야 ^.^</Text>
+            </View>
+            {isEditing ?
+                !isCompleted &&
+                <View style={styles.actions}>
+                    <TouchableOpacity onPress={onClickSave}>
+                        <View style={styles.actionContainer}>
+                            <Text style={styles.actionText}>✅</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                :
+                !isCompleted &&
+                <View style={styles.actions}>
+                    <TouchableOpacity onPress={onClickEditing}>
+                        <View style={styles.actionContainer}>
+                            <Text style={styles.actionText}>✏️</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={onClickDelete}>
+                        <View style={styles.actionContainer}>
+                            <Text style={styles.actionText}>❌</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            }
         </View >
     );
 }
@@ -32,6 +70,7 @@ const styles = StyleSheet.create({
         borderBottomWidth: StyleSheet.hairlineWidth,
         flexDirection: 'row',
         alignItems: 'center',
+        justifyContent: 'space-between',
     },
     circle: {
         width: 30,
@@ -52,5 +91,25 @@ const styles = StyleSheet.create({
     unCompletedCircle: {
         borderColor: '#F23657',
     },
+    completedText: {
+        color: '#bbb',
+        textDecorationLine: 'line-through',
+    },
+    uncompletedText: {
+        color: '#353839',
+    },
+    column: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: width / 2,
+    },
+    actionContainer: {
+        marginVertical: 10,
+        marginHorizontal: 10,
+    },
+    actions: {
+        flexDirection: 'row',
+    }
 });
 export default ToDo;
